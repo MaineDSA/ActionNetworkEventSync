29ad7829517a8bb1b9948226cd8ed036b83f7283
Madison DSA Tech Comm <madisondsatech@gmail.com>
2025-04-30 12:47:00 -0500

    feat: trim more than one <br> to one
    Previous only did pairs of <br>s

--- text.js
+++ text.js
@@ -2,7 +2,7 @@
 // Take a string argument 'description' and format it by replacing various HTML tags and whitespace characters
 function formatDescription (description) {
   return description
-    .replace(/<br><br>/g, '<br>')
+    .replace(/<br>+/g, '<br>')
     .replace(/<p>\s+/g, '<p>')
     .replace(/<br><p>/g, '<p>')
     .replace(/<br><\/p>/g, '</p>')
